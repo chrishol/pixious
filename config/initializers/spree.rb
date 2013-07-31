3fdb456997d58e205afe7f0a9f9b93c3e060489d
Chris Hollindale <chris.hollindale@gmail.com>
2013-07-31 17:49:18 +0100

    Add/change images

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -8,7 +8,7 @@
 Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
-  # config.site_name = "Spree Demo Site"
+  config.site_name = "Pixious - Handmade with Love"
 
   config.use_s3 = true
   config.s3_bucket = 'pixious'
